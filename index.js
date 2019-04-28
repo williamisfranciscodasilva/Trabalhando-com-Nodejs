8544ebd1603cd62ad1b5cde39e0babeea0bc144e
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-04-28 18:20:40 -0300

    configuração do template engine no index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const handlebars = require('express-handlebars');
 
+// Config
+  // Template Engine
+  app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+  app.set('view engine', 'handlebars')
 app.listen(8081, function(){
     console.log("Servidor Rodando na url http://localhost:8081")
 });
