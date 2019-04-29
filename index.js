abc8282db48169d5f791a29777221b329ae2d049
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-04-29 13:23:34 -0300

    Deletando dados e finalizando esse projeto simples com NodeJs

--- index.js
+++ index.js
@@ -34,6 +34,15 @@ const Post = require('./models/Post');
     })
 })
 
+  app.get('/deletar/:id', function(req, res){
+    Post.destroy({where: {'id': req.params.id}})
+    .then(function(){
+      res.send("Postagem deletada com sucesso!")
+    }).catch(function(erro){
+      res.send("Esta postagem não existe! "+erro)
+    })
+  })
+
 app.listen(8081, function(){
     console.log("Servidor Rodando na url http://localhost:8081")
 });
