7febb916e265672d72fbc96542dda801ca6c7726
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-04-29 10:00:05 -0300

    Usando o Post para inserir dados na tabela do banco de dados

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const Post = require('./models/Post');
 
 
 // Config
@@ -13,12 +14,22 @@ const bodyParser = require('body-parser')
   app.use(bodyParser.json())
   
 // Rotas
+  app.get('/', function(req, res){
+    res.render(__dirname +'/views/layouts/home')
+  })
   app.get('/cad', function(req,res){
       res.render(__dirname +'/views/layouts/formulario')
   })
 
   app.post('/add', function(req,res){
-    res.send("Texto: "+req.body.titulo+" Conteudo "+req.body.conteudo)
+    Post.create({
+      titulo: req.body.titulo,
+      conteudo: req.body.conteudo
+    }).then(function(){
+      res.redirect('/')
+    }).catch(function(erro){
+      res.send("Houve um erro: "+erro)
+    })
 })
 
 app.listen(8081, function(){
